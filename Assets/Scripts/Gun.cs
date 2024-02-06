85783a1be1c74de33fb9e0a5f6c068fd94c2a03b
LemonMaster66 <539993@chrome.escg.ac.uk>
2024-02-06 22:57:02 +0000

    Bullet Trails

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -9,48 +9,49 @@ public class Gun : MonoBehaviour
 {
     [Header("References")]
     public GameObject BulletPrefab;
+    public LayerMask  layerMask;
 
     [Header("Types")]
-    public bool Projectile;           // If the fired Bullet is a Projectile
-    public bool Automatic;            // Hold Down the Mouse to Continue Firing
-    public bool DestroyOnImpact;      // Bullets get Destroyed when Colliding
-    public bool ExploadOnImpact;      // Bullets Expload when Colliding
-    public bool ExplodeAfterTime;     // Bullet will auto Explode after an amount of time
-    public bool CanParryBullet;       // Punch after Shooting to Parry Your Own Bullets
-    public bool DamageFallOff;        // The Distance before the Bullets Lose All Damage
-    public bool IgniteEnemies;        // Sets Enemies on Fire
+    public bool Projectile        = false;   // If the fired Bullet is a Projectile
+    public bool Automatic         = false;   // Hold Down the Mouse to Continue Firing
+    public bool DestroyOnImpact   = true;    // Bullets get Destroyed when Colliding
+    public bool ExploadOnImpact   = false;   // Bullets Expload when Colliding
+    public bool ExplodeAfterTime  = false;   // Bullet will auto Explode after an amount of time
+    public bool CanParryBullet    = false;   // Punch after Shooting to Parry Your Own Bullets
+    public bool DamageFallOff     = false;   // The Distance before the Bullets Lose All Damage
+    public bool IgniteEnemies     = false;   // Sets Enemies on Fire
 
     [Header("Properties")]
-    public float Damage;               // Damage on Hit                                             |  0 = None
-    public float MinDamage;            // The Damage Applied at Max Falloff Distance                |  0 = None
-    public int   Ammo;                 // Bullets Shot Before Reloading                             |  0 = Unlimited
-    public float AttackSpeed;          // Time Between Shots                                        |  0 = 1 Frame
-    public float ReloadSpeed;          // Time Taken to Reload                                      |  0 = Instant
-    public int   MultiShot;            // Number Bullets Shot at the Same Time                      |  0 = None
-    public float MultiShotInterval;    // Time Between Each MultiShot Bullet                        |  0 = Instant
-    public float Spread;               // The Amount that Each Bullet Strays from the Target        |  0 = None
-    public float Knockback;            // The Force Applied to the Target from 1 Bullet             |  0 = None
-    public float FalloffDistance;      // The Distance it takes to Lose All Damage                  |  0 = Disabled
-    public float FalloffTime;          // The Time it takes to Lose All Damage                      |  0 = Disabled
-    public int   Ricochete;            // The Number of Times it Bounces before Destroying          |  0 = None
-    public int   Penetrate;            // The Amount of Targets it Pierces Through                  |  0 = None
+    public float Damage;                     // Damage on Hit                                             |  0 = None
+    public float MinDamage;                  // The Damage Applied at Max Falloff Distance                |  0 = None
+    public int   Ammo;                       // Bullets Shot Before Reloading                             |  0 = Unlimited
+    public float AttackSpeed;                // Time Between Shots                                        |  0 = 1 Frame
+    public float ReloadSpeed;                // Time Taken to Reload                                      |  0 = Instant
+    public int   MultiShot;                  // Number Bullets Shot at the Same Time                      |  0 = None
+    public float MultiShotInterval;          // Time Between Each MultiShot Bullet                        |  0 = Instant
+    public float Spread;                     // The Amount that Each Bullet Strays from the Target        |  0 = None
+    public float Knockback;                  // The Force Applied to the Target from 1 Bullet             |  0 = None
+    public float FalloffDistance;            // The Distance it takes to Lose All Damage                  |  0 = Disabled
+    public float FalloffTime;                // The Time it takes to Lose All Damage                      |  0 = Disabled
+    public int   Ricochete;                  // The Number of Times it Bounces before Destroying          |  0 = None
+    public int   Penetrate;                  // The Amount of Targets it Pierces Through                  |  0 = None
 
     [Header("Projectile Properties")]
-    public float ProjectileSpeed;      // The Speed of the Bullet                                   |  0 = Frozen
-    public float ProjectileBounce;     // The Bounciness Value of the Bullet                        |  0 = None
-    public float ProjectileGravity;    // The Gravity Force of the Bullet                           |  0 = None
-    public float ProjectileLifeSpan;   // The Time it takes for the Projectile Dies                 |  0 = Unlimited
+    public float ProjectileSpeed;            // The Speed of the Bullet                                   |  0 = Frozen
+    public float ProjectileBounce;           // The Bounciness Value of the Bullet                        |  0 = None
+    public float ProjectileGravity;          // The Gravity Force of the Bullet                           |  0 = None
+    public float ProjectileLifeSpan;         // The Time it takes for the Projectile Dies                 |  0 = Unlimited
 
     [Header("Explosion Properties")]
-    public float ExplosionSize;        // The Size of the Explosion
-    public float SplashDamage;         // The Max Damage Recieved from being in the Explosion
-    public float ExplosionKnockback;   // The Force Applied to the Target away from the Explosion
+    public float ExplosionSize;              // The Size of the Explosion
+    public float SplashDamage;               // The Max Damage Recieved from being in the Explosion
+    public float ExplosionKnockback;         // The Force Applied to the Target away from the Explosion
 
     [Header("States")]
-    public bool CanShoot = true;
-    public bool AttackCooldown;
-    public bool Reloading;
-    public bool HoldingShoot;
+    public bool CanShoot          = true;
+    public bool AttackCooldown    = false;
+    public bool Reloading         = false;
+    public bool HoldingShoot      = false;
 
     [Header("Info")]
     public float finalDamage;
@@ -97,11 +98,10 @@ public class Gun : MonoBehaviour
         Vector3 shootVector = Camera.transform.forward;
         if(Spread != 0)
         {
-            shootVector.x += UnityEngine.Random.Range(Spread, -Spread);
-            shootVector.y += UnityEngine.Random.Range(Spread, -Spread);
-            shootVector.z += UnityEngine.Random.Range(Spread, -Spread);
+            shootVector.x += UnityEngine.Random.Range(Spread/200, -Spread/200);
+            shootVector.y += UnityEngine.Random.Range(Spread/200, -Spread/200);
+            shootVector.z += UnityEngine.Random.Range(Spread/200, -Spread/200);
         }
-
 
         // Checks
         if(!CanShoot || AttackCooldown) return;
@@ -110,15 +110,19 @@ public class Gun : MonoBehaviour
         AttackCooldown = true;
         attackCooldowntime += AttackSpeed;
 
+        //****************************************************************
+        //Hitscan
         if(!Projectile)
         {
-            RaycastHit hit;
-            if(Physics.Raycast(Camera.transform.position, shootVector, out hit))
+            //**********************************
+            // Hit
+            if(Physics.Raycast(Camera.transform.position, shootVector, out RaycastHit hit, layerMask))
             {
                 finalDamage = Damage;
 
                 GameObject bullet = Instantiate(BulletPrefab, GunTip.position, Quaternion.identity);
-                bullet.transform.position = hit.transform.position;
+                bullet.transform.parent = hit.transform;
+                StartCoroutine(SpawnTrail(bullet, hit.point));
 
                 // Damage Distance Falloff
                 if(FalloffDistance != 0)
@@ -130,6 +134,16 @@ public class Gun : MonoBehaviour
 
                 // Knockback
                 if(hit.rigidbody != null) hit.rigidbody.AddForce(-hit.normal * Knockback *10);
+            }
+
+            //**********************************
+            // Epic Miss
+            else 
+            {
+                finalDamage = 0;
+                GameObject bullet = Instantiate(BulletPrefab, GunTip.position, Quaternion.identity);
+                bullet.transform.parent = hit.transform;
+                StartCoroutine(SpawnTrail(bullet, shootVector*1000));
             }
         }
     }
@@ -146,5 +160,23 @@ public class Gun : MonoBehaviour
     public virtual void AltFireEnded()
     {
         
+    }
+
+    private IEnumerator SpawnTrail(GameObject bullet, Vector3 HitPoint)
+    {
+        float time = 0;
+        Vector3 startPos = bullet.transform.position;
+
+        while(time < 1)
+        {
+            bullet.transform.position = Vector3.Lerp(startPos, HitPoint, time);
+            time += Time.deltaTime*3 / 0.1f;
+            
+            yield return null;
+        }
+        bullet.transform.position = HitPoint;
+        //Spawn Hit Particle
+
+        if(DestroyOnImpact) Destroy(bullet.gameObject, 0.2f);
     }
 }
